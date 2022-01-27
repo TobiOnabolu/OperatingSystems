f3bdf21fb198dbf74a6cd1df7153b0d91bd5dbac
TobiOnabolu <onab4340@mylaurier.ca>
2022-01-26 18:33:38 -0700

    Finsihed question1

--- Assignment1/z_creator.c
+++ Assignment1/z_creator.c
@@ -1,28 +1,29 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-int main(){
-    
-    int lifespan = 20;
-    pid_t pid = fork();
-    //process is a parent
-    if (pid > 0){
-        //Sleep for (lifespan) seconds to allow zombie process to stay alive
-        sleep(lifespan);
-    }
+int main() {
 
-    //process is a child
-    else if (pid == 0){
-        //terminate child process to turn it to zombie
-        exit(0);
-    }
+	int lifespan = 10;
+	int pid = fork();
+	//process is a parent
+	if (pid > 0) {
+		//Sleep for (lifespan) seconds to allow zombie process to stay alive
+		sleep(lifespan);
+	}
 
-    //error occured during fork
-    else {
-        fprintf(stderr, "Failed to Fork");
-        return 1;
-    }
+	//process is a child
+	else if (pid == 0) {
+		//terminate child process to turn it to zombie
+		exit(1);
+	}
 
-    return 0;
+	//error occured during fork
+	else {
+		fprintf(stderr, "Failed to Fork");
+		return 1;
+	}
+
+	return 0;
 }
--- Assignment1/z_terminator.c
+++ Assignment1/z_terminator.c
@@ -1,22 +1,45 @@
 #include <sys/types.h>
 #include <stdio.h>
 #include <unistd.h>
+#include <stdlib.h>
 
 int main() {
-	printf("testing");
 
 	//Create zombie process
 	system("./z_creator &");
+	sleep(2);
+
+	FILE *fp;
+	char ppid[10];
+	char result[20];
 
 	//list all processes running
 	system("ps -l");
 
-	//get zombie process id then get its PPID
-	system("ps -l| grep -w Z|tr -s''|cut -d''-f5");
+	//store ouput from command in file pointer
+	//make sure there is space in between ''
+	fp = popen("ps -l|grep -w Z|tr -s ' '|cut -d ' ' -f 5", "r");
+
+	//error handling
+	if (fp == NULL) {
+		printf("Didnt recieve output from command");
+		exit(1);
+	}
+
+	//get ppid for zombie from commnad
+	fgets(ppid, sizeof(ppid), fp);
+	pclose(fp);
+
+	//format the string to be passed into system call, 1st arg is resulting string
+	sprintf(result, "kill -9 %s", ppid);
+
+	printf("\n---------------Updated Processes--------------------\n");
 
 	//kill parent process by id thats been given by PPID
+	system(result);
 
 	//list all processes again to ensure z process is no longer there
+	system("ps -l");
 
 	return 0;
 }
